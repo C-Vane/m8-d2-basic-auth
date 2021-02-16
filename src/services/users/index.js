92d481e6e702787062c4ffd3b9d0783cb2e60c4e
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2021-02-16 15:22:17 +0100

    Add profile picture

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const UserModel = require("./schema");
 const { adminOnly, basic } = require("../authTools");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "m8-d2",
+    format: async (req, file) => "png" || "jpg",
+    public_id: async (req, file) => req.user.username + "_profile",
+  },
+});
+
+const parser = multer({ storage: storage });
 
 const usersRouter = express.Router();
 
@@ -15,7 +28,7 @@ usersRouter.get("/", basic, adminOnly, async (req, res, next) => {
 
 usersRouter.post("/register", async (req, res, next) => {
   try {
-    const newUser = new UserModel(req.body);
+    const newUser = new UserModel({ image: "https://thumbs.dreamstime.com/b/default-avatar-profile-trendy-style-social-media-user-icon-187599373.jpg", ...req.body });
     const { _id } = await newUser.save();
 
     res.status(201).send(_id);
@@ -39,6 +52,16 @@ usersRouter.put("/me", basic, async (req, res, next) => {
     await req.user.save();
     res.send(req.user);
   } catch (error) {
+    next(error);
+  }
+});
+usersRouter.post("/me/picture", basic, parser.single("image"), async (req, res, next) => {
+  try {
+    req.user.image = req.file.path;
+    await req.user.save();
+    res.status(200).send(req.user);
+  } catch (error) {
+    console.log(error);
     next(error);
   }
 });
